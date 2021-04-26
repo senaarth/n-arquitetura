7e41b7f5c6883c2ced50451664d056f5abffcfea
senaarth <senaarth@gmail.com>
2021-04-26 19:30:55 -0300

    replaces assets (logo and exclamation)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="/images/logo.png" type="image/png" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/png" />
           <style
             dangerouslySetInnerHTML={{
               __html: `
