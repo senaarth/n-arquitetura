24ce43db045c44fb834e934bf12eabcba7b097d6
senaarth <senaarth@gmail.com>
2021-04-26 11:47:13 -0300

    tries to fix font import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,27 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="/images/logo.png" type="image/png"/>
+          <link rel="shortcut icon" href="/images/logo.png" type="image/png" />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+                @font-face {
+                  font-family: "BankGothic Lt BT";
+                  src: url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.eot");
+                  src: url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.eot?#iefix")
+                      format("embedded-opentype"),
+                    url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.woff2")
+                      format("woff2"),
+                    url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.woff")
+                      format("woff"),
+                    url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.ttf")
+                      format("truetype"),
+                    url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.svg#BankGothic Lt BT")
+                      format("svg");
+                }
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
