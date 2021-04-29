2a518df6d5dabb378246fd2a4736f5fd00f052a0
senaarth <senaarth@gmail.com>
2021-04-29 11:03:39 -0300

    adds new social icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { FaWhatsapp, FaLinkedin } from "react-icons/fa";
+import { FaWhatsapp, FaLinkedin, FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
 
 import styles from "./styles.module.scss";
 
@@ -6,17 +6,45 @@ export function Footer() {
   const data = new Date();
   const year = data.toDateString().split(" ")[3];
 
-  return <footer className={styles.footerContainer}>
-    <div className={styles.footerContent}>
-      <div className={styles.contactContent}>
-        <p>N! Arquitetura © {year} |</p>
-        <a href="https://api.whatsapp.com/send?phone=556137973825" target="blank">
-          <FaWhatsapp color="white" size={27.5}/>
-        </a>
-        <a href="https://www.linkedin.com/company/n-arquitetura-gest%C3%A3o-e-neg%C3%B3cios/about/" target="blank">
-          <FaLinkedin color="white" size={27.5}/>
-        </a>
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerContent}>
+        <div className={styles.contactContent}>
+          <div className={styles.socialContainer}>
+            <a
+              href="https://api.whatsapp.com/send?phone=556137973825"
+              target="blank"
+            >
+              <FaWhatsapp color="white" size={27.5} />
+            </a>
+            <a
+              href="#"
+              target="blank"
+            >
+              <FaInstagram color="white" size={27.5} />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/n-arquitetura-gest%C3%A3o-e-neg%C3%B3cios/about/"
+              target="blank"
+            >
+              <FaLinkedin color="white" size={27.5} />
+            </a>
+            <a
+              href="#"
+              target="blank"
+            >
+              <FaFacebook color="white" size={27.5} />
+            </a>
+            <a
+              href="#"
+              target="blank"
+            >
+              <FaTwitter color="white" size={27.5} />
+            </a>
+          </div>
+          <p>N! Arquitetura © {year}</p>
+        </div>
       </div>
-    </div>
-  </footer>;
+    </footer>
+  );
 }
