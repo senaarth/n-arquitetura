eb7c421068d49969105b9709744a823ae1a9a2ce
senaarth <senaarth@gmail.com>
2021-04-30 15:15:40 -0300

    fixes footer icons proportions

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { FaWhatsapp, FaLinkedin, FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
+import { FaWhatsapp, FaLinkedin, FaInstagram, FaFacebook, FaYoutube } from "react-icons/fa";
 
 import styles from "./styles.module.scss";
 
@@ -15,31 +15,31 @@ export function Footer() {
               href="https://api.whatsapp.com/send?phone=556137973825"
               target="blank"
             >
-              <FaWhatsapp color="white" size={27.5} />
+              <FaWhatsapp color="white" size={19.25} />
             </a>
             <a
               href="#"
               target="blank"
             >
-              <FaInstagram color="white" size={27.5} />
+              <FaInstagram color="white" size={19.25} />
             </a>
             <a
               href="https://www.linkedin.com/company/n-arquitetura-gest%C3%A3o-e-neg%C3%B3cios/about/"
               target="blank"
             >
-              <FaLinkedin color="white" size={27.5} />
+              <FaLinkedin color="white" size={19.25} />
             </a>
             <a
               href="#"
               target="blank"
             >
-              <FaFacebook color="white" size={27.5} />
+              <FaYoutube color="white" size={19.25} />
             </a>
             <a
               href="#"
               target="blank"
             >
-              <FaTwitter color="white" size={27.5} />
+              <FaFacebook color="white" size={19.25} />
             </a>
           </div>
           <p>N! Arquitetura © {year}</p>
