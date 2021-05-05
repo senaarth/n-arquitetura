477869499061421fff768a6b5942b81b03c6dd4d
senaarth <senaarth@gmail.com>
2021-05-04 22:25:32 -0300

    fixes font loading delay

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,25 +6,11 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" type="image/png" />
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `
-                @font-face {
-                  font-family: "BankGothic Lt BT";
-                  src: url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.eot");
-                  src: url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.eot?#iefix")
-                      format("embedded-opentype"),
-                    url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.woff2")
-                      format("woff2"),
-                    url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.woff")
-                      format("woff"),
-                    url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.ttf")
-                      format("truetype"),
-                    url("//db.onlinewebfonts.com/t/8058c5fac0f4c6d0d692921f5637319a.svg#BankGothic Lt BT")
-                      format("svg");
-                }
-              `,
-            }}
+          <link
+            rel="preload"
+            href="/fonts/BankGothicLtBt/BankGothic_Lt_BT.ttf"
+            as="font"
+            crossOrigin=""
           />
         </Head>
         <body>
