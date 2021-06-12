e53cc73850e8d1868f225dd45e9172e83e3c3815
senaarth <senaarth@gmail.com>
2021-06-11 21:43:29 -0300

    connects variedades page to prismic

--- src/pages/variedades.tsx
+++ src/pages/variedades.tsx
@@ -1,89 +1,36 @@
 import { MainContainer } from "../components/MainContainer";
 import Head from "next/head";
 import React, { useState } from "react";
+import { GetStaticProps } from "next";
+import getPrismicClient from "../services/prismic";
+import { RichText } from "prismic-dom";
+import Prismic from "@prismicio/client";
+
+type Variedade = {
+  place: number;
+  title: string;
+  contentTitle: string;
+  description: string;
+};
+
+interface VariedadesProps {
+  variedades: Variedade[];
+}
 
 interface ContentProps {
   title?: string;
   description?: string;
 }
 
-export default function Clipping() {
+export default function Variedades({ variedades }: VariedadesProps) {
   const [contentProps, setContentProps] = useState({} as ContentProps);
 
-  const clipping = (
-    <>
-      <li style={{ listStyle: "none" }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <h5 style={{ margin: 0 }}>Lorem Ipsum</h5>
-          <a style={{ marginLeft: 10, color: "var(--red)", cursor: "pointer" }}>
-            Ver Mais
-          </a>
-        </div>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
-      </li>
-      <li style={{ listStyle: "none" }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <h5 style={{ margin: 0 }}>Lorem Ipsum</h5>
-          <a style={{ marginLeft: 10, color: "var(--red)", cursor: "pointer" }}>
-            Ver Mais
-          </a>
-        </div>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
-      </li>
-      <li style={{ listStyle: "none" }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <h5 style={{ margin: 0 }}>Lorem Ipsum</h5>
-          <a style={{ marginLeft: 10, color: "var(--red)", cursor: "pointer" }}>
-            Ver Mais
-          </a>
-        </div>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
-      </li>
-    </>
-  );
-
-  const menuItems = [
+  const emptyMenu = [
     {
-      title: "CLIPPING",
+      title: "",
       isActive: false,
       isLink: false,
-      type: "mobile",
-      content: {
-        description: clipping,
-      },
+      type: "text",
     },
     {
       title: "",
@@ -136,6 +83,33 @@ export default function Clipping() {
     },
   ];
 
+
+  const variedadesContent = variedades.reduce((acc, item) => {
+
+
+    return [
+      ...acc,
+      {
+        title: item.title,
+        isActive: false,
+        isLink: false,
+        type: "mobile",
+        content: {
+          title: item.contentTitle,
+          description: item.description,
+        },
+      },
+    ];
+  }, []);
+
+  let menuItems = emptyMenu;
+
+  variedades.forEach((value, index) => {
+    if (value.place !== 4 && value.place !== 5) {
+      menuItems[value.place - 1] = variedadesContent[index];
+    }
+  });
+
   return (
     <>
       <Head>
@@ -149,3 +123,39 @@ export default function Clipping() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const res = await prismic.query(
+    [Prismic.predicates.at("document.type", "variedades")],
+    {
+      pageSize: 7,
+    }
+  );
+
+  const variedades = res.results.map((post) => {
+    return {
+      place: post.data.place,
+      title: RichText.asText(post.data.title),
+      contentTitle: RichText.asText(post.data.content_title),
+      description: RichText.asHtml(post.data.description),
+    };
+  });
+
+  variedades.sort(function (a, b) {
+    if (a.place > b.place) {
+      return 1;
+    }
+    if (a.place < b.place) {
+      return -1;
+    }
+    return 0;
+  });
+
+  return {
+    props: {
+      variedades,
+    },
+  };
+};
