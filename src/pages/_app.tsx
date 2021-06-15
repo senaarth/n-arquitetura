86b6c04dcbe6666fb0775ecc6a773a856a889867
senaarth <senaarth@gmail.com>
2021-06-14 21:27:14 -0300

    creates warning page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import Image from "next/image";
+import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Header } from "../components/Header";
@@ -9,6 +10,40 @@ import { Footer } from "../components/Footer";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  if (!process.env.NEXT_PUBLIC_IS_DEVELOPMENT) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <img
+          src="/static/images/exclamacao.png"
+          style={{
+            width: "70%",
+            maxWidth: "360px",
+            marginTop: "auto",
+          }}
+        />
+        <p
+          style={{
+            marginBottom: "auto",
+            textAlign: "center",
+            marginTop: "2rem",
+            fontSize: "1.675rem",
+            maxWidth: "90%"
+          }}
+        >
+          OBRIGADO POR NOS VISITAR!<br/><br/>ESTAMOS FINALIZANDO UM CONTEÚDO BEM LEGAL
+          PARA VOCÊ.<br/><br/>NOS ENCONTRAMOS AQUI NOVAMENTE NO DIA 05/07.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
       <MobileHeader />
